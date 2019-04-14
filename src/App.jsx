073dccc00ccf5891e7df5c98d38c49f164ab5f7e
Simon Gomes <busy.s.simon@gmail.com>
2019-04-14 23:27:21 +0600

    tick/audio added

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,79 @@
-import React, { Component } from "react";
-import Slider from "rc-slider";
-import { AwesomeButton } from "react-awesome-button";
-import "rc-slider/assets/index.css";
-import "react-awesome-button/dist/themes/theme-c137.css";
+import React, { Component } from 'react';
+import Slider from 'rc-slider';
+import { AwesomeButton } from 'react-awesome-button';
+import 'rc-slider/assets/index.css';
+import 'react-awesome-button/dist/themes/theme-c137.css';
+// Import audio
+import tick from './audio/tick.wav';
+import tock from './audio/tock.wav';
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            bpm: 1,
-            buttonActive: false,
-            bpmToMillisecond: 60000
-        };
+  constructor() {
+    super();
+    this.state = {
+      bpm: 1,
+      playing: false,
+      bpmToMillisecond: 60000,
+      ticker: 0,
+      tick: [new Audio(tick), new Audio(tock)]
+    };
 
-        // Bind functions
-        this.onBpmChange = this.onBpmChange.bind(this);
-        this.onButtonClick = this.onButtonClick.bind(this);
-    }
-    render() {
-        return (
-            <div>
-                <p>{this.state.bpm}</p>
-                <Slider
-                    onChange={this.onBpmChange}
-                    min={1}
-                    max={200}
-                    disabled={this.state.buttonActive}
-                />
-                <AwesomeButton
-                    type="primary"
-                    size="medium"
-                    className="start-button"
-                    onPress={this.onButtonClick}
-                >
-                    {!this.state.buttonActive ? "START" : "STOP"}
-                </AwesomeButton>
-            </div>
-        );
-    }
+    // Bind functions
+    this.onBpmChange = this.onBpmChange.bind(this);
+    this.onButtonClick = this.onButtonClick.bind(this);
+  }
+  render() {
+    const { bpm, playing } = this.state;
+    return (
+      <div className="metronome-wrap">
+        <p>{bpm}</p>
+        <Slider
+          onChange={this.onBpmChange}
+          min={1}
+          max={200}
+          disabled={playing}
+        />
+        <AwesomeButton
+          type="primary"
+          size="medium"
+          className="start-button"
+          onPress={this.onButtonClick}
+        >
+          {playing ? 'START' : 'STOP'}
+        </AwesomeButton>
+      </div>
+    );
+  }
 
-    // Update BPM
-    onBpmChange(bpmUpdate) {
-        this.setState({
-            bpm: bpmUpdate,
-            bpmToMillisecond: 60000 / bpmUpdate
-        });
-    }
+  // Update BPM
+  onBpmChange(bpmUpdate) {
+    this.setState({
+      bpm: bpmUpdate,
+      bpmToMillisecond: 60000 / bpmUpdate
+    });
+  }
 
-    // Activate and Deactivate
-    onButtonClick() {
-        this.setState(prevState => ({ buttonActive: !prevState.buttonActive }));
-        if (this.state.buttonActive === true) {
-            clearInterval(this.timerID);
-        } else {
-            this.timerID = setInterval(
-                () => this.beep(),
-                this.state.bpmToMillisecond
-            );
-        }
+  // Activate and Deactivate
+  onButtonClick() {
+    this.setState(({ playing }) => ({ playing: !playing }));
+    if (this.state.playing === true) {
+      clearInterval(this.timerID);
+    } else {
+      this.timerID = setInterval(
+        () => this.beep(),
+        this.state.bpmToMillisecond
+      );
     }
+  }
 
-    beep() {
-        console.log(
-            "beep" +
-                Math.random()
-                    .toString(36)
-                    .substring(7)
-        );
-    }
+  // Play the dound
+  beep() {
+    const { tick, ticker } = this.state;
+    tick[ticker].play();
+
+    // Switch the ticker index to play tick and tock
+    this.setState(({ ticker }) => ({ ticker: ticker * -1 + 1 }));
+  }
 }
 
 export default App;
